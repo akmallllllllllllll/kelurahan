c3ce7dc5e84571d5f0c9edee9135e8c5f838449c
akmallllllllllllll <akmal.farhan2004@gmail.com>
2025-10-07 09:36:48 +0700

    commit kedua

---
+++ data_kelurahan/warga/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Warga, Pengaduan
+
+class WargaListView(ListView):
+    model = Warga
+
+class WargaDetailView(DetailView):
+    model = Warga
+
+class PengaduanListView(ListView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_list.html'  # arahkan ke template baru
+    context_object_name = 'pengaduan_list'        # nama variabel di template
