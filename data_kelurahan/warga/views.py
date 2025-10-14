c05b99f183760658ac9cda3f8abe67fa3745b9da
akmallllllllllllll <akmal.farhan2004@gmail.com>
2025-10-14 11:01:35 +0700

    commit ketiga

--- data_kelurahan/warga/views.py
+++ data_kelurahan/warga/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Warga, Pengaduan
+from .forms import WargaForm, PengaduanForm
+
 
 class WargaListView(ListView):
     model = Warga
@@ -12,3 +15,15 @@ class PengaduanListView(ListView):
     model = Pengaduan
     template_name = 'warga/pengaduan_list.html'  # arahkan ke template baru
     context_object_name = 'pengaduan_list'        # nama variabel di template
+
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list')
+
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
