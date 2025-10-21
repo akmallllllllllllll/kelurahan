af97fb16c42b86f2cb5918cbc8c7bbf44f29503e
akmallllllllllllll <akmal.farhan2004@gmail.com>
2025-10-21 09:53:11 +0700

    commit keempat

--- data_kelurahan/warga/urls.py
+++ data_kelurahan/warga/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView, PengaduanCreateView
+from .views import WargaListView, WargaDetailView, PengaduanListView, WargaCreateView, PengaduanCreateView, WargaUpdateView, WargaDeleteView
 
 urlpatterns = [
     path('', WargaListView.as_view(), name='warga-list'),
     path('tambah/', WargaCreateView.as_view(), name='warga-tambah'),
     path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
     path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
-    path('pengaduan/tambah/', PengaduanCreateView.as_view(), name='pengaduan-tambah'),  # 🔽 Tambahan
+    path('pengaduan/tambah/', PengaduanCreateView.as_view(), name='pengaduan-tambah'),
+    path('<int:pk>/edit/', WargaUpdateView.as_view(), name='warga-edit'),  
+    path('<int:pk>/hapus/', WargaDeleteView.as_view(), name='warga-hapus'),  # 🔽 Tambahan
 ]
 
